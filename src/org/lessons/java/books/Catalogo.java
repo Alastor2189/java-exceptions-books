969c3d0637aa68d8639211e9e6541af96d1d3b1d
Alastor2189 <g.lucafarci89@gmail.com>
2023-01-31 19:23:56 +0100

    Completamento esercizio

--- src/org/lessons/java/books/Catalogo.java
+++ src/org/lessons/java/books/Catalogo.java
@@ -15,7 +15,7 @@ public class Catalogo {
 		 do {
 			 try {
 				 	System.out.print("Digita il numero di libri che vuoi inserire:");
-				 	numPagine = Integer.parseInt(s.nextLine());
+				 	n = Integer.parseInt(s.nextLine());
 				 	if(n < 1 || n > 50 )
 				 		System.out.println("Numero non corretto, inserire un numero compreso fra 1 e 50");
 				 	
@@ -25,6 +25,34 @@ public class Catalogo {
 		 } while (n < 1 || n > 50);
 		 
 		 Libri [] catalogo = new Libri[n];
+		 
+		 for (int i = 0; i < n; i++) {
+			 System.out.println("Inserisci il titolo del libro:");
+			 titolo = s.nextLine();
+			 System.out.println("Inserisci ora il numero delle pagine");
+			 try {
+				 numPagine = Integer.parseInt(s.nextLine());
+			 } catch (Exception l) {
+				 numPagine = 0;
+			 }
+			 
+			 System.out.print("Scrivi il nominativo dell'autore:");
+			 autore = s.nextLine();
+			 System.out.println("Scrivi il nome della casa editrice:");
+			 editore = s.nextLine();
+			 
+			 Libri testo;
+			 try {
+				 testo = new Libri(titolo, numPagine, autore, editore);
+				 catalogo[i] = testo;
+			 }catch (Exception l) {
+				 l.getMessage();
+			 }
+			 
+			 System.out.println("Libro registrato" + catalogo[i].toString());
+		 }
+		 
+		 s.close();
 		 
 	}
 }
